79e7f08ed9783d6f73f04900b450d5732ed9fd64
Faizan Ahmad <87941590+InnovatorBrain@users.noreply.github.com>
2023-07-07 10:30:11 +0000

    available_books

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,2 +1,32 @@
 // Write your code here
-console.log("Hello, Faizan !");
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability(); // Return the result of getAvailability() instead of calling the method without returning.
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of Stock";
+    } else if (this.numCopies < 10) {
+      return "Low Stock";
+    } else {
+      return "In Stock";
+    }
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5) {
+    // Corrected the method name to "restock" instead of "resticked".
+    this.numCopies += numCopiesStocked;
+  }
+}
