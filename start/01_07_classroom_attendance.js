ab005f053a0b5bb9d1ef089a1011b9325f4902dc
Faizan Ahmad <87941590+InnovatorBrain@users.noreply.github.com>
2023-07-07 17:23:28 +0000

    classroomAttendance

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
-// Write your code here
+// Destructuring (Extract values from objects and arrays into variables);
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+  if (hasTeachingAssistant) {
+    ({ teacher, teachingAssistant, ...students } = classList);
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(
+  getStudents({
+    hasTeachingAssistant: false,
+    classList: ["Faizan", "Ahmad", "student1", "student2", "student3"],
+  })
+);
