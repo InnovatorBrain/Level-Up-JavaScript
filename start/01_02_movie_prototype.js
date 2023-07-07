739da4f0821d88d9ec07a2437a8423be6b4ac02f
Faizan Ahmad <87941590+InnovatorBrain@users.noreply.github.com>
2023-07-07 12:17:12 +0000

    2nd challenge

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,33 @@
-// Write your code here
+// Declairing Object with function method:
+// function Movie(title, director, genre, releaseYear, rating) {
+//   this.title = title;
+//   this.director = director;
+//   this.genre = genre;
+//   this.releaseYear = releaseYear;
+//   this.rating = rating;
+// }
+// Movie.prototype.getOverview = function () {
+//   return `${this.title}, a ${this.genre} film directted by ${this.director}, released in ${this.releaseYear}. It recieved a ratting of ${this.rating}`;
+// };
+// const John_Wick = new Movie("John Wick", "legend", "action", 2023);
+// const Batman = new Movie("The Dark Night", "legend,action", 2023);
+// console.log(John_Wick.getOverview());
+// console.log(Batman.getOverview());
+// _______________________________________________
+// I can also declair an object in class method
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+  getOverview = function () {
+    return `${this.title}, a ${this.genre} film directted by ${this.director}, released in ${this.releaseYear}. It recieved a ratting of ${this.rating}`;
+  };
+}
+const John_Wick = new Movie("John Wick", "legend", "action", 2023);
+const Batman = new Movie("The Dark Night", "legend,action", 2023);
+console.log(John_Wick.getOverview());
+console.log(Batman.getOverview());
