ee49ae261f82e3750153c45e44eca1d6fea9fbea
Faizan Ahmad <87941590+InnovatorBrain@users.noreply.github.com>
2023-07-08 05:42:07 +0000

    coffeeCalculation

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,8 @@
-// Write your code here
+// Coffee function:
+function coffeeDate(arr) {
+  let coffeeSum = arr.reduce(
+    (totalCoffee, numCoffee) => (totalCoffee += numCoffee)
+  );
+  return `You will have to pay: $ ${coffeeSum * 1.25}`;
+}
+console.log(coffeeDate([5, 7, 3, 8, 1]));
