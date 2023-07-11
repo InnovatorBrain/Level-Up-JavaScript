120c4e36ad8f942d7e25f5aad1b59c0ede0c674b
Faizan Ahmad <87941590+InnovatorBrain@users.noreply.github.com>
2023-07-11 18:05:55 +0000

    passwordStrength

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,20 @@
-// Write your code here
+// PASSWORD STRENGTH:
+// REGULAR EXPRESSION:
+// USER PASSWORD IS STRENGTHEND OR NOT
+function checkPassword(password) {
+  let oneLowerCaseLetter = /(?=.*[a-z])/;
+  let oneUpperCaseLetter = /(?=.*[A-Z])/;
+  let oneDigit = /(?=.*[0-9])/;
+  let oneSpecialCharacter = /(?=.*[!@#$%^&*])/;
+  let minimumEightCharacters = /(?=.{8,})/;
+  let isValid =
+    oneLowerCaseLetter.test(password) &&
+    oneUpperCaseLetter.test(password) &&
+    oneDigit.test(password) &&
+    oneSpecialCharacter.test(password) &&
+    minimumEightCharacters.test(password);
+  return isValid ? "Your password is valid" : "Your password is invalid";
+}
+
+console.log(checkPassword("abc"));
+console.log(checkPassword("9Ab!4567"));
