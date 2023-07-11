c19752a50a854baaab2ee20dd21a34ad2ecee715
Faizan Ahmad <87941590+InnovatorBrain@users.noreply.github.com>
2023-07-11 12:22:20 +0000

    fiveFriends

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,23 @@
-// Write your code here
+// IMPORTANT FOR INTERVIEWS
+// String Permutation | Recursion
+// function numPermutations(letters) {
+//   let total;
+//   for (let i = 1; i <= letters.length; i++) {
+//     if (total === undefined) {
+//       total = i;
+//       continue;
+//     }
+//     total = i;
+//   }
+//   return total;
+//   // The code after this point is missing
+// }
+// console.log(numPermutations("hello"));
+// _________________________________________________
+// Other method for Permutation
+function numPermutations(letters) {
+  if (letters.length === 1) {
+    return 1;
+  }
+  return letters.length * numPermutations(letters.slice(1));
+}
